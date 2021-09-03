09b85e5dd6e0ee652366eeee24cde95358eb7b26
talnoznisky <tal.noznisky@gmail.com>
2021-09-03 16:08:28 -0500

    improved enchantment

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ BEGIN_TEXT =    'BEGIN DIVINATION...'
 END_TEXT =      '...END DIVINATION.'
 
 
+def _delete_previous_line():
+    sys.stdout.write("\033[F") #back to previous line 
+    sys.stdout.write("\033[K") #clear line 
+
 def _handle_dashes(spread: str):
     options = ["-"," "]
     add = options["-" in spread]
@@ -19,16 +23,12 @@ def _handle_dashes(spread: str):
  
     return spread.replace(remove, add)
 
-
-def spinner():
-    spinner = itertools.cycle(['|','/','-','\\'])
-    
-    for _ in range(30):
-        sys.stdout.write(next(spinner))
+def magic():
+    l = itertools.cycle(['✨','🔮'])
+    for _ in range(12):
+        time.sleep(0.2)
+        sys.stdout.write(next(l))
         sys.stdout.flush()
-        time.sleep(0.1)
-        sys.stdout.write('\b')
-
 
 def get_spread():
     spread_names = [_handle_dashes(spread) for spread in spreads]
@@ -45,22 +45,35 @@ def get_spread():
 
 def output(selection, spread):
     sys.stdout.write('\n')
+    
     sys.stdout.write(f'YOU HAVE SELECTED: {selection}\n')
-    time.sleep(0.3)
-    sys.stdout.write(f'🔮✨OK, HOLD UP, DOING TAROT✨🔮 ')
-    spinner()
     sys.stdout.write('\n')
+    time.sleep(0.5)
+    
+    
+    sys.stdout.write(f'OK, HOLD UP, DOING TAROT\n')
+    sys.stdout.write('\n')
+    
+    magic()
+    sys.stdout.write('\n')
+    sys.stdout.write('\n')
+    time.sleep(0.5)
 
-    sys.stdout.write('\n')
-    sys.stdout.write(BEGIN_TEXT+'\n')
-    sys.stdout.write('\n')
+    sys.stdout.write('###BEGIN DIVINATION TRANSCRIPT### \n')
+    sys.stdout.write('\n')    
+    time.sleep(0.5)
 
     for card in spread:
         sys.stdout.write(f'{card["position"].upper()}: {card["name"] +" (REVERSED)" if card["reversed"] else card["name"]}'+'\n')
         sys.stdout.write(card["description"]+'\n')     
+        
         sys.stdout.write('\n')
+        input('Press Enter to continue...')
+        _delete_previous_line()
+        sys.stdout.write('\n')
+        
     
-    sys.stdout.write(END_TEXT)
+    sys.stdout.write('###DIVINATION TRANSCRIPT END###')
     sys.stdout.write('\n\n')
 
 if __name__ == "__main__":
